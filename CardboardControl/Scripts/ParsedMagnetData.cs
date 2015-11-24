897f2bb87cef5df99632c06f7f2239a175bfc3a4
JScott <jvscott@gmail.com>
2015-11-23 23:07:29 -0800

    Completely reworked how magnet data is parsed

--- CardboardControl/Scripts/ParsedMagnetData.cs
+++ CardboardControl/Scripts/ParsedMagnetData.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /**
 * Dealing with raw magnet input from a Cardboard device
@@ -9,31 +10,95 @@ public class ParsedMagnetData {
 
   private float accelerationMagnitude;
   private float gyroRotationMagnitude;
-  private float magneticFieldMagnitude;
-  private float magneticFieldBaseLine;
-  private float magneticFieldRatio;
 
   private const int slowImpulseFilter = 25;
   private const int fastMagnetImpulseFilter = 3;
   private const int fastRotationImpulseFilter = 3;
   private const int fastAccelerationImpulseFilter = 5;  // Clicking the magnet tends to make a small, sharp spike in movement
 
+  private struct MagnetMoment {
+  	public float deltaTime;
+  	public float yMagnitude;
+    public MagnetMoment(float deltaTime, float yMagnitude) {
+      this.deltaTime = deltaTime;
+      this.yMagnitude = yMagnitude;
+    }
+  }
+  private struct MagnetWindowState {
+    public float firstHalf;
+    public float lastHalf;
+    public float ratio; // TODO: do I only need this?
+  }
+  private List<MagnetMoment> magnetWindow;
+  private MagnetWindowState currentMagnetWindow;
+  private float MAX_WINDOW_SECONDS = 0.4f;
+  private float MAGNET_RATIO_THRESHOLD = 0.1f;
+  private float windowLength = 0.0f;
+
   public ParsedMagnetData() {
     Input.compass.enabled = true;
     Input.gyro.enabled = true;
     accelerationMagnitude = Input.acceleration.magnitude;
     gyroRotationMagnitude = Input.gyro.rotationRate.magnitude;
-    magneticFieldMagnitude = magneticFieldBaseLine = Input.compass.rawVector.magnitude;
+    magnetWindow = new List<MagnetMoment>();
+    windowLength = 0.0f;
   }
 
   public void Update() {
     Vector3 accelerationDelta = CalculateAccelerationDelta();
     Vector3 gyroRotation = Input.gyro.rotationRate;
-    Vector3 magneticField = Input.compass.rawVector;
 
-    FilterAndSetMagnitudes(accelerationDelta.magnitude, gyroRotation.magnitude, magneticField.magnitude);
-    magneticFieldRatio = (magneticFieldMagnitude / magneticFieldBaseLine);
-  	Debug.Log("MFRatio: "+magneticFieldRatio);
+    FilterAndSetMagnitudes(accelerationDelta.magnitude, gyroRotation.magnitude);
+    TrimMagnetWindow();
+    AddToMagnetWindow();
+    currentMagnetWindow = CaptureMagnetWindow();
+
+    // Debug.Log(Input.compass.rawVector.x + "\n" +
+    //           Input.compass.rawVector.y + "\n" +
+    //           Input.compass.rawVector.z + "\n" +
+    //           Input.compass.rawVector.magnitude + "\n");
+    // Debug.Log(currentMagnetWindow.firstHalf + "\n" +
+    //           currentMagnetWindow.lastHalf + "\n" +
+    //           currentMagnetWindow.ratio);
+  }
+
+  public void TrimMagnetWindow() {
+    while (windowLength > MAX_WINDOW_SECONDS) {
+      MagnetMoment moment = magnetWindow[0];
+      magnetWindow.RemoveAt(0);
+      windowLength -= moment.deltaTime;
+    }
+  }
+
+  public void AddToMagnetWindow() {
+    magnetWindow.Add(new MagnetMoment(Time.deltaTime, Input.compass.rawVector.magnitude));
+    windowLength += Time.deltaTime;
+  }
+
+  private MagnetWindowState CaptureMagnetWindow() {
+    MagnetWindowState newState = new MagnetWindowState();
+    int middle = magnetWindow.Count / 2;
+    List<MagnetMoment> firstHalf = magnetWindow.GetRange(0, middle);
+    List<MagnetMoment> lastHalf = magnetWindow.GetRange(middle, magnetWindow.Count - middle);
+    newState.firstHalf = Average(firstHalf);
+    newState.lastHalf = Average(lastHalf);
+    newState.ratio = newState.firstHalf / newState.lastHalf;
+    return newState;
+  }
+
+  private float Average(List<MagnetMoment> moments) {
+    if (moments.Count == 0) return 0.0f;
+    float sum = 0.0f;
+    for (int index = 0; index < moments.Count; index++) {
+      sum += moments[index].yMagnitude;
+    }
+    return sum / moments.Count;
+  }
+
+  private bool IsMagnetGoingDown(float min, float max, float start) {
+    float minDelta = Mathf.Abs(min - start);
+    float maxDelta = Mathf.Abs(max - start);
+    return minDelta > maxDelta;
   }
 
   private Vector3 CalculateAccelerationDelta() {
@@ -48,14 +113,12 @@ public class ParsedMagnetData {
   }
 
   // TODO: dictionaries could shorten a lot of these lines
-  private void FilterAndSetMagnitudes(float newAccelerationMagnitude, float newGyroRotationMagnitude, float newMagneticFieldMagnitude) {
+  private void FilterAndSetMagnitudes(float newAccelerationMagnitude, float newGyroRotationMagnitude) {
     // Apply Finite Impulse Response (FIR) filters
     // If the tilt hasn't changed, but the compass has, then the magnetic field moved without the device
     // This is the essence of a cardboard magnet click
     accelerationMagnitude = ImpulseFilter(accelerationMagnitude, newAccelerationMagnitude, fastAccelerationImpulseFilter);
     gyroRotationMagnitude = ImpulseFilter(gyroRotationMagnitude, newGyroRotationMagnitude, fastRotationImpulseFilter);
-    magneticFieldMagnitude = ImpulseFilter(magneticFieldMagnitude, newMagneticFieldMagnitude, fastMagnetImpulseFilter);
-    magneticFieldBaseLine = ImpulseFilter(magneticFieldBaseLine, newMagneticFieldMagnitude, slowImpulseFilter);
   }
 
   public bool IsCalibrating() {
@@ -71,10 +134,10 @@ public class ParsedMagnetData {
   }
 
   public bool IsDown() {
-    return magneticFieldRatio < 0.95;
+    return currentMagnetWindow.ratio > 1.0f+MAGNET_RATIO_THRESHOLD;
   }
 
   public bool IsUp() {
-    return magneticFieldRatio > 1.05;
+    return currentMagnetWindow.ratio < 1.0f-MAGNET_RATIO_THRESHOLD;
   }
 }
