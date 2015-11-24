2ac6028de79e31928a1c62ae43a9402dcdb8eb9e
JScott <jvscott@gmail.com>
2015-11-23 23:46:39 -0800

    Fixing touch controls

--- CardboardControl/Scripts/ParsedTouchData.cs
+++ CardboardControl/Scripts/ParsedTouchData.cs
@@ -22,11 +22,17 @@ public class ParsedTouchData {
   }
 
   public bool IsDown() {
+    // touchCount can jump for no reason in a Cardboard
+    // but it's too quick to be considered "Moved"
+    return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved;
+  }
+
+  public bool StillDown() {
     return Input.touchCount > 0;
   }
 
   public bool IsUp() {
-    if (!IsDown() && wasTouched) {
+    if (!StillDown() && wasTouched) {
       wasTouched = false;
       return true;
     }
