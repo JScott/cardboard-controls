25199ac0be42d5cb27d6a6dd0e1ce2feb735e90b
JScott <jvscott@gmail.com>
2015-12-08 23:15:00 -0800

    A basic pointer object

---
+++ CardboardControl/Scripts/CardboardControlPointer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardControlPointer : MonoBehaviour {
+	private GameObject pointer;
+  public GameObject pointerPrefab;
+  public LayerMask raycastIgnoreLayer = 1 << Physics.IgnoreRaycastLayer;
+
+  void Start () {
+    pointer = Instantiate(pointerPrefab) as GameObject;
+    GameObject head = GameObject.Find("CardboardMain/Head");
+    pointer.transform.parent = head.transform;
+    pointer.layer = LayerMask.NameToLayer("Ignore Raycast");
+	}
+
+  // TODO: public bool startHidden = false;
+  // TODO: void Hide()
+  // TODO: void Show()
+  // TODO: void ChangeColor(Color color)
+  // NOTE: Be sure to fade alpha/colors, never abruptly jump
+}
